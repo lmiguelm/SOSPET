f740f2b4d99e4166c883efe9d97fd2010c354c6e
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-06-03 10:30:56 -0300

    Projeto Criado

---
+++ src/main/java/com/lmiguel/sospet/SospetApplication.java
@@ -0,0 +1,13 @@
+package com.lmiguel.sospet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SospetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SospetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmiguel/sospet/SospetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmiguel.sospet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SospetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
